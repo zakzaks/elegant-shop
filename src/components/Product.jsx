4ab4f63ea6f85f17bc7d002ce558657efe8c01d9
Arief Nugroho <ariefn82@gmail.com>
2026-04-01 19:25:08 +0700

    Add shopping cart context and integrate components
    Introduce a React shopping cart context (src/store/shopping-cart-context.jsx) to centralize cart state and actions and avoid prop drilling. Update App.jsx, Shop.jsx, Product.jsx and Cart.jsx to consume the new context for adding/removing items and displaying cart data, simplifying component logic and state management.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,24 +1,22 @@
-export default function Product({
-  id,
-  image,
-  title,
-  price,
-  description,
-  onAddToCart,
-}) {
-  return (
-    <article className="product">
-      <img src={image} alt={title} />
-      <div className="product-content">
-        <div>
-          <h3>{title}</h3>
-          <p className='product-price'>${price}</p>
-          <p>{description}</p>
-        </div>
-        <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
-        </p>
-      </div>
-    </article>
-  );
+import { use } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Product({ id, image, title, price, description }) {
+	const { addItemToCart } = use(CartContext);
+
+	return (
+		<article className="product">
+			<img src={image} alt={title} />
+			<div className="product-content">
+				<div>
+					<h3>{title}</h3>
+					<p className="product-price">${price}</p>
+					<p>{description}</p>
+				</div>
+				<p className="product-actions">
+					<button onClick={() => addItemToCart(id)}>Add to Cart</button>
+				</p>
+			</div>
+		</article>
+	);
 }
